f3f590c66949ca2ce605c78aa7605eec2bff79cf
Rhelcris Salvino de Sousa <rhelcris@gmail.com>
2020-06-27 23:15:54 -0300

    Alterando classe principal

--- src/main/java/br/com/itau/catapi/CatBreedInfoApiApplication.java
+++ src/main/java/br/com/itau/catapi/CatBreedInfoApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatApiApplication {
+public class CatBreedInfoApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatApiApplication.class, args);
+		SpringApplication.run(CatBreedInfoApiApplication.class, args);
 	}
 
 }
