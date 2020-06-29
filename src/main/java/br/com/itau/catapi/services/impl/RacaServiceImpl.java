f98ee7c58f486cc80b9c150b8a4dff0f5b24373c
Rhelcris Salvino de Sousa <rhelcris@gmail.com>
2020-06-29 20:45:40 -0300

    Finalizando para entregar

--- src/main/java/br/com/itau/catapi/services/impl/RacaServiceImpl.java
+++ src/main/java/br/com/itau/catapi/services/impl/RacaServiceImpl.java
@@ -3,14 +3,17 @@ package br.com.itau.catapi.services.impl;
 import br.com.itau.catapi.beans.Raca;
 import br.com.itau.catapi.repositories.RacaRepository;
 import br.com.itau.catapi.services.RacaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class RacaServiceImpl implements RacaService {
+
+    private static final Logger logger = LoggerFactory.getLogger(RacaServiceImpl.class);
 
     private RacaRepository racaRepository;
 
@@ -21,6 +24,7 @@ public class RacaServiceImpl implements RacaService {
 
     @Override
     public Optional<Raca> buscarPeloCondigo(String id) {
+        logger.info("Consulta raça pelo ID: " + id);
         return this.racaRepository.findById(id);
     }
 
